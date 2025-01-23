cd60e1133da78dd435efa8ec1e91aa49b73b1659
Dhruva Prasad <dhruvaprasad004@gmail.com>
2025-01-23 20:51:26 +0530

    directing base route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,24 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
-import { Signin } from './pages/Signin'
-import { Blog } from './pages/Blog'
-import { Signup } from './pages/Signup'
-import { Blogs } from "./pages/Blogs"
-import { Publish } from './pages/Publish'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import './App.css';
+import { Signin } from './pages/Signin';
+import { Blog } from './pages/Blog';
+import { Signup } from './pages/Signup';
+import { Blogs } from './pages/Blogs';
+import { Publish } from './pages/Publish';
 
 function App() {
-  return <>
+  return (
     <BrowserRouter>
-        <Routes>
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/signin" element={<Signin />} />
-            <Route path="/blog/:id" element={<Blog />} />
-            <Route path="/blogs" element={<Blogs />} />
-            <Route path="/publish" element={<Publish />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Navigate to="/signin" replace />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/blog/:id" element={<Blog />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/publish" element={<Publish />} />
+      </Routes>
     </BrowserRouter>
-  </>
+  );
 }
 
-export default App
+export default App;
